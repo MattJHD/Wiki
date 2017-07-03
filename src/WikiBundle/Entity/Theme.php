80de53bac902078127150d92b47b68cd1069bbcd
Matthieu <contact@matthieudurand.fr>
2017-07-03 13:04:48 +0200

    first entity

---
+++ src/WikiBundle/Entity/Theme.php
@@ -0,0 +1,19 @@
+<?php
+namespace WikiBundle\Entity;
+
+
+use Doctrine\ORM\Mapping as ORM;
+use JMS\Serializer\Annotation\Type;
+use Symfony\Component\Validator\Constraints as Assert;
+
+
+/**
+ * Description of Theme
+ *
+ * @author matthieudurand
+ * @ORM\Entity(repositoryClass="WikiBundle\Repository\ThemeRepository")
+ * @ORM\Table()
+ */
+class Theme {
+    
+}
---
+++ src/WikiBundle/Repository/ThemeRepository.php
@@ -0,0 +1,13 @@
+<?php
+namespace WikiBundle\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+/**
+ * Description of ThemeRepository
+ *
+ * @author matthieudurand
+ */
+class ThemeRepository extends EntityRepository{
+    
+}
