5a1373abc0f6497c0cf2ab68244fa69c721bcfba
Matthieu <contact@matthieudurand.fr>
2017-07-05 15:06:02 +0200

    authentication json web tokens

--- src/WikiBundle/Controller/DefaultController.php
+++ src/WikiBundle/Controller/DefaultController.php
@@ -4,14 +4,24 @@ namespace WikiBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/wiki")
+     * @Route("/")
      */
     public function indexAction()
     {
         return $this->render('default/index.html.twig');
+    }
+    
+    /**
+     * @Route("/test", name="test")
+     */
+    public function apiAction(Request $request)
+    {
+        return new Response(sprintf('Logged in as %s', $this->getUser()->getUsername()));
     }
 }
