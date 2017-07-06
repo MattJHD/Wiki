82e28dd77297af6d5d015227dae852b517e341a5
Matthieu <contact@matthieudurand.fr>
2017-07-06 15:35:19 +0200

    GET one Article + Edit

--- web/wiki_angularjs/app/modules/dashboard/controllers/articles/ArticlesController.js
+++ web/wiki_angularjs/app/modules/dashboard/controllers/articles/ArticlesController.js
@@ -1,5 +1,6 @@
 dashboard.controller("ArticlesController", ['$scope', '$http', 'appSettings', '$mdDialog',
 	function($scope, $http, appSettings, $mdDialog){
+
 		var backend = appSettings.backend;
 		console.log(backend);
 
@@ -10,41 +11,117 @@ dashboard.controller("ArticlesController", ['$scope', '$http', 'appSettings', '$
 		});
 
 		$scope.status = '  ';
-		$scope.customFullscreen = false;
+  		$scope.customFullscreen = true;
 
-		$scope.showAdvanced = function(ev, id) {
-	    $mdDialog.show({
-	      controller: ModalController,
-	      templateUrl: 'app/modules/dashboard/views/articles/articleDetail.html',
-	      parent: angular.element(document.body),
-	      targetEvent: ev,
-	      clickOutsideToClose:true,
-	      fullscreen: $scope.customFullscreen, // Only for -xs, -sm breakpoints.
-				data: {
-					id:id
+		//détails article
+		$scope.showArticle = function($event, id){
+			
+			$mdDialog.show({
+	          	locals: {id: id},
+	            controller: modalController,
+	            templateUrl: 'app/modules/dashboard/views/articles/articleDetails.html',
+	            parent: angular.element(document.body),
+	            targetEvent: $event,
+	            clickOutsideToClose:true,
+	            fullscreen: $scope.customFullscreen // Only for -xs, -sm breakpoints.
+	          })
+	          .then(function(answer) {
+	            $scope.status = 'ok';
+	          });
+
+          	function modalController($scope, $mdDialog, $http, id) {
+				$http.get(backend + "articles/" + id).then(function(response){
+					$scope.thisArticle = response.data;
+					console.log($scope.thisArticle);
+				});
+
+				$scope.hide = function() {
+				  $mdDialog.hide();
+				};
+
+				$scope.cancel = function() {
+				  $mdDialog.cancel();
+				};
+			}
+
+        };
+
+    //edit article
+	$scope.editArticle = function($event, id){
+		
+		$mdDialog.show({
+          	locals: {id: id},
+            controller: modalEditController,
+            templateUrl: 'app/modules/dashboard/views/articles/articleEdit.html',
+            parent: angular.element(document.body),
+            targetEvent: $event,
+            clickOutsideToClose:true,
+            fullscreen: $scope.customFullscreen // Only for -xs, -sm breakpoints.
+          })
+          .then(function(answer) {
+            $scope.status = 'ok';
+          });
+
+      	function modalEditController($scope, $mdDialog, $http, id) {
+			$http.get(backend + "articles/" + id).then(function(response){
+				$scope.thisArticle = response.data;
+				console.log($scope.thisArticle);
+			});
+
+			$http.get(backend + "themes").then(function(response){
+				$scope.themes = response.data;
+				console.log($scope.themes);
+			});
+
+			$scope.hide = function() {
+			  $mdDialog.hide();
+			};
+
+			$scope.cancel = function() {
+			  $mdDialog.cancel();
+			};
+
+			$scope.goEdit = function(thisArticle){
+				console.log(thisArticle);
+				var themes = [];
+				for(var i = 0; i<thisArticle.themes.length; i++){
+					var dataTheme = JSON.parse(thisArticle.themes[i]); 
+					themes.push(dataTheme);
 				}
-			})
-	    .then(function(answer) {
-	      $scope.status = 'You said the information was "' + answer + '".';
-	    }, function() {
-	      $scope.status = 'You cancelled the dialog.';
-	    });
-	  };
 
-		function ModalController($scope, $mdDialog, id) {
+				var dataArticle = new Object();
+					dataArticle.id = id;
+					dataArticle.name = thisArticle.name;
+					dataArticle.description = thisArticle.description;
+					dataArticle.date_creation = thisArticle.date_creation;
+					dataArticle.pathname = "";
+					dataArticle.user = {};
+					dataArticle.themes = themes;
 
-			console.log('id = '+id);
-	    $scope.hide = function() {
-	      $mdDialog.hide();
-	    };
+				var jsonArticle = JSON.stringify(dataArticle);
+				console.log(jsonArticle);
 
-	    $scope.cancel = function() {
-	      $mdDialog.cancel();
-	    };
+				$http({
+					url: backend + "articles/" + id, 
+					method: 'PUT',
+					data: jsonArticle, 
+					headers: { 'content-type': 'application/json' }
+				}).success(function(data, status, headers, config, answer){
+					$scope.PostDataResponse = data;
+					console.log($scope.PostDataResponse);
+					location.reload();
+				}).error(function (data, status, header, config) {
+	                $scope.ResponseDetails = "Data: " + data +
+	                    "<hr />status: " + status +
+	                    "<hr />headers: " + header +
+	                    "<hr />config: " + config;
+	            });
 
-	    $scope.answer = function(answer) {
-	      $mdDialog.hide(answer);
-	    };
-	  }
+
+
+			}
+		}
+
+    };
 
 }]);
