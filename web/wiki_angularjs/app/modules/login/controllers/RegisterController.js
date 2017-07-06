a3447c8a8365e1fa6ac2fef72cab1ba281122bc0
Svanaks <bloch.william@gmail.com>
2017-07-06 15:25:23 +0200

    check email @hitema.fr

--- web/wiki_angularjs/app/modules/login/controllers/RegisterController.js
+++ web/wiki_angularjs/app/modules/login/controllers/RegisterController.js
@@ -3,52 +3,39 @@ login.controller("registerController", ['$rootScope', '$scope', '$state', '$http
 
 	  $scope.user = {};
 		$scope.register = function(user) {
-			if(user.password === user.confirmPassword){
-				// console.log(user);
 
+			console.log(user.email);
+			var idx = user.email.lastIndexOf('@');
+			if (idx > -1 && user.email.slice(idx+1) === 'hitema.fr') {
+			  //true if the address ends with hitema.fr
+				if(user.password === user.confirmPassword){
+					// console.log(user);
+					let parameter = JSON.stringify({id:"", role:{name:"Abonne", users:[], id:"3"}, firstname:user.firstname, lastname: user.lastname, username:user.username, salt:"", password:user.password, email: user.email, articles:[]});
+					let backend = appSettings.backend;
+					let url = backend + "creation";
 
-				let parameter = JSON.stringify({id:"", role:{name:"Abonne", users:[], id:"3"}, firstname:user.firstname, lastname: user.lastname, username:user.username, salt:"", password:user.password, email: user.email, articles:[]});
-				let backend = appSettings.backend;
-				let url = backend + "creation";
-
-				console.log(parameter);
-					// We POST
-			    $http.post(url, parameter).
-			    success(function(data, status, headers, config) {
-			        $state.go('home');
-			        console.log(data);
-			      }).
-			      error(function(data, status, headers, config) {
-			        // called asynchronously if an error occurs
-			        // or server returns response with an error status.
-							console.log('erreur');
-							console.log(data);
-							console.log(status);
-							console.log(header);
-			      });
-					//console.log(parameter);
+					console.log(parameter);
+						// We POST
+				    $http.post(url, parameter).
+				    success(function(data, status, headers, config) {
+				        $state.go('home');
+				        console.log(data);
+				      }).
+				      error(function(data, status, headers, config) {
+				        // called asynchronously if an error occurs
+				        // or server returns response with an error status.
+								console.log('erreur');
+								console.log(data);
+								console.log(status);
+								console.log(header);
+				      });
+						//console.log(parameter);
+				} else {
+					alert('Les mots de passe doivent être identiques');
+				}
 			} else {
-				alert('Les mots de passe doivent être identiques');
+				alert('Votre adresse email doit terminer par hitema.fr');
 			}
+
     };
 }]);
-
-
-// Posting data to php file
-// $http({
-//   method  : 'POST',
-//   url     : backend + "users",
-//   data    : $scope.user, //forms user object
-//   headers : {'Content-Type': 'application/x-www-form-urlencoded'}
-//  })
-//   .success(function(data) {
-//     if (data.errors) {
-//       // Showing errors.
-//       $scope.errorName = data.errors.name;
-//       $scope.errorUserName = data.errors.username;
-//       $scope.errorEmail = data.errors.email;
-//     } else {
-//       $scope.message = data.message;
-//     }
-//   });
-// console.log($scope.user);
