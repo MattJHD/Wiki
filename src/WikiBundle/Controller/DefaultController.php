08652c39ab8751a39ed46874c3de85ea9ee2d905
Matthieu <contact@matthieudurand.fr>
2017-07-07 14:48:57 +0200

    contact mail

--- src/WikiBundle/Controller/DefaultController.php
+++ src/WikiBundle/Controller/DefaultController.php
@@ -4,8 +4,10 @@ namespace WikiBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class DefaultController extends Controller
 {
@@ -24,4 +26,22 @@ class DefaultController extends Controller
     {
         return new Response(sprintf('Logged in as %s', $this->getUser()->getUsername()));
     }
+    
+    /**
+    * @Route("/contact")
+    * @Method("POST")
+    */
+     public function contactAction(Request $request){
+         
+        $jsonData = json_decode($request->getContent(), true);
+        
+        $name = $jsonData['lastname'];
+        $firstName = $jsonData['firstname'];
+        $email = $jsonData['email'];
+        $body = $jsonData['message'];
+
+        $this->get('mailer.contact_mailer')->sendMail($name, $firstName, $email, $body);
+        
+        return new JsonResponse('mail sent');
+     }
 }
